e82b401af3cd2a1a1a69ea88b314171a47b33ae7
Tom <tom.beusch@stud.fh-campuswien.ac.at>
2022-03-17 12:54:27 +0100

    rowtests implemented

---
+++ AxiUnitTests/Classes/RowTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using AxiLogic.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AxiUnitTests.Classes
+{
+    [TestClass]
+    public class RowTests
+    {
+        [TestMethod]
+        public void TestRemoveRack()
+        {
+            //arrange
+            var row = new Row();
+            var rack = new Rack();
+            row.AddRack(rack);
+            //act
+            row.RemoveRack(rack);
+            //assert
+            Assert.IsTrue(row.GetRacks().Count == 0);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRemoveNotContainedRack()
+        {
+            //arrange
+            var row = new Row();
+            var rack = new Rack();
+            var rack2 = new Rack();
+            row.AddRack(rack);
+            //act
+            row.RemoveRack(rack2);
+            //assert
+            Assert.IsTrue(row.GetRacks().Count == 1);
+        }
+        
+        [TestMethod]
+        public void TestAddRack()
+        {
+            //arrange
+            var row = new Row();
+            var rack = new Rack();
+            //act
+            row.AddRack(rack);
+            //assert
+            Assert.IsTrue(row.GetRacks().Contains(rack),"Rack was not added");
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAddDuplicateRack()
+        {
+            //arrange
+            var row = new Row();
+            var rack = new Rack();
+            row.AddRack(rack);
+            //act
+            row.AddRack(rack);
+            //assert
+            Assert.IsTrue(row.GetRacks().Count == 1, "Adding duplicate was allowed");
+        }
+        
+        [TestMethod]
+        public void TestCreateRow()
+        {
+            //arrange
+            //act
+            var row = new Row();
+            //assert
+            Assert.IsNotNull(row);
+        }
+
+        [TestMethod]
+        public void TestSetName()
+        {
+            //arrange
+            var row = new Row();
+            var name = "someName";
+            //act
+            row.SetName(name);
+            //assert
+            Assert.AreEqual(name, row.Name);
+        }
+    }
+}
