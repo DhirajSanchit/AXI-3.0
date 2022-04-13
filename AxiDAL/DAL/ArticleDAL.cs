b5579a60ca28e81baac5b517ec7b7f9a4ecd3144
Bjongen <488751@student.fontys.nl>
2022-04-13 14:34:55 +0200

    edited Article queries

--- AxiDAL/DAL/ArticleDAL.cs
+++ AxiDAL/DAL/ArticleDAL.cs
@@ -11,7 +11,8 @@ namespace AxiDAL.DAL
 {
     public class ArticleDAL : IArticleDAL
     {
-
+        private const string Connectionstring =
+            "Server=mssqlstud.fhict.local;Database=dbi484674;User Id=dbi484674;Password=DatabaseAXItim;";
         private IDbConnection _dbConnection;
         private IList<ArticleDto> _dataset;
         
@@ -58,24 +59,118 @@ namespace AxiDAL.DAL
         }
         
         
-        public ArticleDto GetById()
+        public ArticleDto GetByBarcode()
         {
-            throw new System.NotImplementedException();
+            
+            using var connection = new SqlConnection(Connectionstring);
+            connection.Open();
+
+            const string sql = "Select * From [Article] Where Barcode = @Barcode";
+
+            ArticleDto articleDto = new();
+            using (var cmd = new SqlCommand(sql, connection))
+            {
+                cmd.Parameters.AddWithValue("@Barcode", articleDto.Id);
+            }
+
+            connection.Close();
+            return articleDto;
         }
 
         public bool AddArticle(ArticleDto articleDto)
         {
-            using var connection = new SqlConnection
+            const string sql = "insert into [Article] values(@Name, @Price, @ImgRef, @Category, @Description)";
+            try
+            {
+                using (_dbConnection)
+                {
+                    var result = _dbConnection.Execute(sql, new 
+                        {
+                         @Name = articleDto.Name,
+                         @Price = articleDto.Price,
+                         @ImgRef = articleDto.ImgRef,
+                         @Category = articleDto.Category,
+                         @Description = articleDto.Description 
+                        });
+                    //possibly replace bool with int rowsAffected?
+                    return true;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception(ex.Message);
+            }
+
+            finally
+            {
+                _dbConnection.Close();
+            }
         }
 
-        public bool UpdateArticle()
+        public bool UpdateArticle(ArticleDto articleDto)
         {
-            throw new NotImplementedException();
+            const string sql = "Update [Article] " +
+                "Set [Name] = @Name," +
+                "[Price] = @Price," +
+                "[ImgRef] = @ImgRef " +
+                "[Category] = @Category " +
+                "[Description] = @Description " +
+                "Where Barcode = @Barcode";
+
+            try
+            {
+                using (_dbConnection)
+                {
+                    var result = _dbConnection.Execute(sql, new
+                    {
+                        @Name = articleDto.Name,
+                        @Price = articleDto.Price,
+                        @ImgRef = articleDto.ImgRef,
+                        @Category = articleDto.Category,
+                        @Description = articleDto.Description,
+                        @Barcode = articleDto.Barcode
+                    });
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                _dbConnection.Close();
+            }
         }
 
-        public bool DeleteArticle()
+        public bool DeleteArticle(ArticleDto articleDto)
         {
-            throw new NotImplementedException();
+            const string sql = "Delete from [Article] " +
+                "Where Barcode = @Barcode";
+
+            try
+            {
+                using(_dbConnection)
+                {
+                    var result = _dbConnection.Execute(sql, new
+                    {
+                        @Barcode = articleDto.Barcode
+                    });
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                _dbConnection.Close();
+            }
         }
     }
 }
