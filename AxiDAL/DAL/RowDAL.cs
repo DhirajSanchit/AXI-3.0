58b13a97e348d9b4b3252c862ee76da9bce47af2
Bjongen <488751@student.fontys.nl>
2022-04-25 14:49:41 +0200

    created RowDAL

--- AxiDAL/DAL/RowDAL.cs
+++ AxiDAL/DAL/RowDAL.cs
@@ -1,8 +1,139 @@
-﻿using AxiDAL.Interfaces;
+﻿using AxiDAL.DTOs;
+using AxiDAL.Interfaces;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace AxiDAL.DAL
 {
     public class RowDAL : IRowDAL
     {
+        private IDbConnection _dbConnection;
+        private IList<ArticleDto> _dataset;
+        //private const string connectionstring = "Server=mssqlstud.fhict.local;Database=dbi484674;User Id = dbi484674; Password=DatabaseAXItim;";
+
+        //Assign connectionstring from appsettings.json
+        public RowDAL(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+
+        //Retrieves all Rows
+        public IList<ArticleDto> GetAll()
+        {
+            //Prepare Query
+            var sql = @"SELECT * FROM [Row]";
+
+            //Execute statement
+            try
+            {
+                using (_dbConnection)
+                {
+                    //Execute query on Database, and return _dataset
+                    _dataset = (IList<ArticleDto>)_dbConnection.Query<RowDto>(sql).ToList();
+                    return _dataset;
+                }
+            }
+
+            //TODO: Catch and handle possible exceptions
+            //Catches possible exceptions
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                throw new Exception(ex.Message);
+            }
+
+            //Closes DB connection when finishing statement regardless of result(s)
+            finally
+            {
+
+                _dbConnection.Close();
+            }
+        }
+        public bool AddRow(RowDto rowDto)
+        {
+            const string sql = "insert into [Row] values(@Name)";
+            try
+            {
+                using (_dbConnection)
+                {
+                    var result = _dbConnection.Execute(sql, new
+                    {
+                        @Name = rowDto.Name,
+                        
+                    });
+                    //possibly replace bool with int rowsAffected?
+                    return true;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception(ex.Message);
+            }
+
+            finally
+            {
+                _dbConnection.Close();
+            }
+        }
+        public bool UpdateArticle(RowDto rowDto)
+        {
+            const string sql = "Update [Row] " +
+                "Set [Name] = @Name";
+
+            try
+            {
+                using (_dbConnection)
+                {
+                    var result = _dbConnection.Execute(sql, new
+                    {
+                        @Name = rowDto.Name,
+                    });
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                _dbConnection.Close();
+            }
+        }
+
+        public bool DeleteArticle(RowDto rowDto)
+        {
+            const string sql = "Delete from [Row] " +
+                "Where Name = @Name";
+
+            try
+            {
+                using (_dbConnection)
+                {
+                    var result = _dbConnection.Execute(sql, new
+                    {
+                        @Name = rowDto.Name
+                    });
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                _dbConnection.Close();
+            }
+        }
     }
 }
