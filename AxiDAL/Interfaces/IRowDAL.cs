0497ebc3c59220dd94b86bebcf8dc4462a3399c8
Bjongen <488751@student.fontys.nl>
2022-05-11 10:48:27 +0200

    Update IRowDAL.cs

--- AxiDAL/Interfaces/IRowDAL.cs
+++ AxiDAL/Interfaces/IRowDAL.cs
@@ -1,7 +1,16 @@
+using AxiDAL.DTOs;
+using System.Collections.Generic;
+
 namespace AxiDAL.Interfaces
 {
     public interface IRowDAL
     {
-        
+        public IList<ArticleDto> GetAll();
+
+        public bool AddRow(RowDto rowDto);
+
+        public bool UpdateArticle(RowDto rowDto);
+
+        public bool DeleteArticle(RowDto rowDto);
     }
 }
