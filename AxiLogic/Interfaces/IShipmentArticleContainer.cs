451d077f1942b94f7b845f41ea906afce4f52900
Bjongen <488751@student.fontys.nl>
2022-05-12 14:04:56 +0200

    shipmentarticle functions made

---
+++ AxiLogic/Interfaces/IShipmentArticleContainer.cs
@@ -0,0 +1,16 @@
+﻿using AxiLogic.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxiLogic.Interfaces
+{
+    public interface IShipmentArticleContainer
+    {
+        public IReadOnlyCollection<ShipmentArticle> GetShipmentArticles();
+        public void ClearShipmentArticles();
+        public List<IShipmentArticleDto>
+    }
+}
