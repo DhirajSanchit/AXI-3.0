4218fe1e4c7ff0c743590c5ddb0ab692fa42e404
Saeryun3 <samed19982008@hotmail.com>
2022-06-09 15:10:12 +0200

    mock fix 3.0

--- AxiUnitTests/Mocks/EmployeeMock.cs
+++ AxiUnitTests/Mocks/EmployeeMock.cs
@@ -7,6 +7,34 @@ namespace AxiUnitTests.Scrubs // to do
 {
     public class EmployeeMock : IEmployeeDAL
     {
+        private IDalFactory mockdalfactory;
+        public List<EmployeeDto> employees = new List<EmployeeDto>();
+
+        public EmployeeMock(IDalFactory dalFactory)
+        {
+            mockdalfactory = dalFactory;
+            var employee1 = new EmployeeDto()
+            {
+                Id = 1,
+                Email = "Test@gmail.com",
+                Name = "Name1",
+                Description = "Placeholder",
+                EmployeeNr = "A1042",
+                PhoneNr = "+3168912341",
+            }; 
+            var employee2 = new EmployeeDto()
+            {
+                Id = 2,
+                Email = "Testemployee@gmail.com",
+                Name = "Name2",
+                Description = "Placeholders",
+                EmployeeNr = "A1044",
+                PhoneNr = "+3168915748",
+            };
+            employees.Add(employee1);
+            employees.Add(employee2);
+        }
+
         public IList<EmployeeDto> GetAll()
         {
             throw new System.NotImplementedException();
